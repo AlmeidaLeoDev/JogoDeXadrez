e6fb5c766e91392c5dcb1b6e2be842d371db34bc
Leonardo Almeida <almeidaleo.dev@gmail.com>
2023-07-22 23:10:30 -0300

    Melhorando a implementação do método imprimirTabuleiro

--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -8,6 +8,7 @@ namespace Xadrez_Console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.WriteLine(8 - i + "");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null) //Se não houver nenhuma peça na posição
@@ -16,11 +17,27 @@ namespace Xadrez_Console
                     }
                     else
                     {
-                        //Chamando o método peça no objeto tab
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.WriteLine(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branco) //Se for branco imprimi
+            {
+                Console.Write(peca);
+            }
+            else //Se for preto imprimi como amarelo
+            {
+                //ConsoleColor é um tipo do C# que pega a cor do sistema
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
--- Xadrez-Console/xadrez/PosicaoXadrez.cs
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using tabuleiro;
 
 namespace Xadrez_Console.xadrez
 {
     internal class PosicaoXadrez
     {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        //Converter a posição do xadrez (a1, b2, c6...) para posição interna da matriz
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
     }
 }
